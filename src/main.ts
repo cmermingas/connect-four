3c8b7899a60953f7d1b16bd0471fedb57856f4b9
cmermingas <cmermingas@gmail.com>
2016-08-10 00:41:30 -0400

    Re-added web animations polyfill in index.html because the standard process doesn't work in prod.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import 'web-animations-js';
+// Removing this import since it doesn't work in prod
+// import 'web-animations-js';
 import { AppComponent, environment } from './app/';
 
 if (environment.production) {
